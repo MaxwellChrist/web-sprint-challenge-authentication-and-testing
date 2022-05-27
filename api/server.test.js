31f6cab60b8ebf205c05b8fa77540addac48a9c4
MaxwellChrist <ribthulu@gmail.com>
2022-05-27 15:29:21 -0400

    created 7 tests in server.test.js

--- api/server.test.js
+++ api/server.test.js
@@ -2,7 +2,8 @@
 const request = require('supertest');
 const server = require('./server')
 const db = require('../data/dbConfig')
-const Users = require('./api/auth/auth-model')
+const Users = require('../api/auth/auth-model')
+const bcrypt = require('bcryptjs')
 
 const user1 = { username: 'aaa', password: 'fff'}
 const user2 = { username: 'bbb', password: 'ggg'}
@@ -18,6 +19,10 @@ afterAll(async () => {
   await db.destroy()
 })
 
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
@@ -27,8 +32,70 @@ test('running on testing environment', () => {
 })
 
 describe('testing API endpoints', () => {
-  test('#1 POST /api/auth/register', async () => {
-    const result = await Users.
+  test('#1 - POST /api/auth/register - basic check to see if post request can be made', async () => {
+    let result = await request(server).post('/api/auth/register').send({ username: 'aaa', password: '111'})
+    expect(result.status).toBe(200)
+    expect(result.body).toMatchObject({ id: 1, username: 'aaa'})
+    expect(result.status).not.toBe(400)
+    expect(result.body).not.toMatchObject({ id: 2, username: 'bbb'})
+  })
+  test('#2 - POST /api/auth/register - check password is stored as hash in correct username', async () => {
+    let result = await request(server).post('/api/auth/register').send({ username: 'bbb', password: '222'})
+    let input = await db('users').where('username', 'bbb').first()
+    expect(bcrypt.compareSync('222', input.password)).toBeTruthy()
+    expect(input).toMatchObject({ id: 1, username: 'bbb', password: input.password })
+  })
+  test('#3 - POST /api/auth/register - checking missingUsernameOrPassword middleware', async () => {
+    let result
+    result = await request(server).post('/api/auth/register').send({ username: '', password: '333'})
+    expect(result.status).toBe(400)
+    expect(result).not.toHaveProperty('username', '')
+    result = await request(server).post('/api/auth/register').send({ username: 'ccc', password: ''})
+    expect(result.status).toBe(400)
+    expect(result).not.toHaveProperty('username', 'ccc')
+    expect(result.body.message).toBe("username and password required")
+  })
+  test('#4 - POST /api/auth/register - checking alreadyTakeUsername middleware', async () => {
+    let result
+    result = await request(server).post('/api/auth/register').send({ username: 'aaa', password: '111'})
+    expect(result.status).toBe(200)
+    expect(result.body).toMatchObject({ id: 1, username: 'aaa'})
+    result = await request(server).post('/api/auth/register').send({ username: 'aaa', password: '222'})
+    expect(result.status).toBe(400)
+    expect(result.body).not.toMatchObject({ id: 1, username: 'aaa'})
+    expect(result.body.message).toBe("username taken")
+  })
+  test('#5 - POST /api/auth/login - basic check to see if post request can be made', async () => {
+    let result
+    result = await request(server).post('/api/auth/register').send({ username: 'aaa', password: '111'})
+    expect(result.status).toBe(200)
+    expect(result.body).toMatchObject({ id: 1, username: 'aaa'})
+    result = await request(server).post('/api/auth/login').send({ username: 'aaa', password: '111'})
+    expect(result.status).toBe(200)
+    expect(result.body.message).toBe("welcome, aaa",)
+    expect(result.body.token).toBeDefined()
+  })
+  test('#6 - POST /api/auth/login - checking missingUsernameOrPassword middleware - username', async () => {
+    let result
+    result = await request(server).post('/api/auth/register').send({ username: 'aaa', password: '111'})
+    expect(result.status).toBe(200)
+    expect(result.body).toMatchObject({ id: 1, username: 'aaa'})
+    result = await request(server).post('/api/auth/login').send({ username: '', password: '111'})
+    expect(result.status).toBe(400)
+    expect(result.body.message).not.toBe("welcome, aaa",)
+    expect(result.body.message).toBe("username and password required")
+  })
+  test('#7 - POST /api/auth/login - checking missingUsernameOrPassword middleware - password', async () => {
+    let result
+    result = await request(server).post('/api/auth/register').send({ username: 'bbb', password: '222'})
+    expect(result.status).toBe(200)
+    expect(result.body).toMatchObject({ id: 1, username: 'bbb'})
+    result = await request(server).post('/api/auth/register').send({ username: 'bbb', password: ''})
+    expect(result.status).toBe(400)
+    expect(result.body.message).not.toBe("welcome, aaa",)
+    expect(result.body.message).toBe("username and password required")
+  })
+  test('#8 - POST /api/auth/login - checking usernameExistsOrPasswordInvalid middleware', async () => {
 
   })
 })
